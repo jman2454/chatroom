d110f2abe6351049f3383a603e7ec21e0e25a9e1
James Meyers <jwm337@cornell.edu>
2020-06-16 02:34:12 -0400

    users chatting working

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bruh'
 socketio = SocketIO(app)
+
+users = {}
 
 
 @app.route('/')
@@ -11,11 +13,29 @@ def index():
     return render_template('index.html')
 
 
-@socketio.on('message')
-def test_message(message):
-    print(message)
-    send(message, broadcast=True)
+@app.route('/user', methods=["GET"])
+def name():
+    try:
+        return users[request.args.get('socketid')]
+    except:
+        return ""
+
+
+@socketio.on('push')
+def test_message(id, message):
+    emit("pull",
+         users[id] + ": " + message + "<br>", broadcast=True)
+
+
+@socketio.on('new user')
+def add_user(id, name):
+    if not id in users:
+        print(name)
+        users[id] = name
+        emit('new user success')
+    else:
+        emit('already registered', 'You are already registered!')
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host="0.0.0.0", port="5000")
--- static/js/chat.js
+++ static/js/chat.js
@@ -2,17 +2,51 @@
 // var io = require('socket.io-client');
 $(document).ready(function () {
 
-  var socket = io.connect("http://127.0.0.1:5000")
+  var socket = io.connect("http://10.0.0.25:5000")
+
+  $.ajax(
+    {
+      url: window.location.href + "user",
+      socketid: socket.id,
+      success: function (response) {
+        if (response === "") {
+          $("#name").html(
+            '<input id="namebox" placeholder="enter a username"></input>'
+          );
+        } else {
+          $("#name").html(
+            "<p>Welcome, " + response + "!</p>"
+          );
+        }
+      }
+    }
+  );
+
 
   socket.on('connect', function () {
-    socket.send('User connected!');
-  })
+    console.log("connected");
+    // socket.emit('new user', socket.id);
+  });
 
-  socket.on('message', function (msg) {
+  socket.on('pull', function (msg) {
     $("#msgs").append(msg);
-  })
+  });
 
   $("#send").click(function () {
-    $("#msgs").html($("#msgs").html() + $("#msgbox").val());
-  })
+    socket.emit('push', socket.id, $("#msgbox").val());
+  });
+
+  $("#name").on('keyup', function (e) {
+    if (e.keyCode === 13) {
+      socket.emit('new user', socket.id, $("#namebox").val());
+    }
+  });
+
+  socket.on('already registered', function (msg) {
+    alert(msg);
+  });
+
+  socket.on('new user success', function () {
+    $("#name").html("");
+  });
 })
