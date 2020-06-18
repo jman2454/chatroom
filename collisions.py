f44bdcb3b37a8018ee611b392a4239a0113a1789
James Meyers <jwm337@cornell.edu>
2020-06-18 19:25:36 -0400

    test'

---
+++ collisions.py
@@ -0,0 +1,20 @@
+
+
+class Collisions:
+    def __init__(self):
+        pass
+
+    def update(self, players):
+        plist = [players[k] for k in players]
+        for i in range(len(plist)):
+            for o in range(i + 1, len(plist)):
+                p = plist[i]
+                p2 = plist[o]
+                if p != p2 and p.collides(p2):
+                    diffVec = p.getPos().cpy().sub(p2.getPos()).nor()
+                    dist = p.getPos().dist(p2.getPos())
+                    v1 = p.getVel().mag()
+                    v2 = p2.getVel().mag()
+                    totalVel = v1 + v2
+                    p.setPosRelative(diffVec.cpy().times(dist * v1/totalVel))
+                    p2.setPosRelative(diffVec.cpy().times(-dist * v2/totalVel))
